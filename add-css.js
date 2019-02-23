abe19c341c351b96b7a3ca824594f544c7b59d59
Andrew Arensburger <arnie@lala>
2019-02-22 22:12:46 -0500

    Move the CSS-generating code out into add-css.js

--- add-css.js
+++ add-css.js
@@ -6,3 +6,30 @@ function add_css(css_text) {
     css.innerHTML = css_text;
     head.appendChild(css);
 }
+
+// set_colors
+// Takes a hash of the form:
+//	{
+//	  selector: [fg_color, bg_color],
+//	  ...
+//	}
+// converts it to a CSS string, and adds it to the document.
+// Here, 'selector' is a CSS selector (e.g. an element or class), and
+// 'fg_color' and 'bg_color' are the foreground and background colors,
+// respctively.
+function set_colors(color_table)
+{
+    let css = "";
+    for (var sel in color_table)
+    {
+	let fg = color_table[sel][0];
+	let bg = color_table[sel][1];
+	css +=
+`${sel} {
+	color: ${fg};
+	background-color: ${bg};
+}
+`;
+    }
+    add_css(css);
+}
--- tweak-css.user.js
+++ tweak-css.user.js
@@ -6,12 +6,21 @@
 // @require  add-css.js
 // @run-at   document-idle
 // ==/UserScript==
-var fg = "red";
-var bg = "yellow"
 
-add_css(
-`body {
-    background-color: ${bg};
-    color:	${fg};
-}
-`);
+// Colors:
+// Define the color palette we'll be using. These are just variables to
+// make it easier to repeat colors.
+// These colors are from http://paletton.com/#uid=53U0m0kitPxdtXDgdUkkdIKln-9
+var primary_1    = "#7180e2";
+var secondary1_1 = "#8a6fe3";
+var secondary2_1 = "#67a6df";
+
+// Define which areas of the page should have which colors.
+// The key is a CSS selector, which can be an ID, a class, etc.
+// The value is a two-valued array, with the foreground and background
+// color.
+// Can use either CSS color strings, or the variables, above.
+set_colors({
+    "body":	["black", primary_1],
+    ".navbox":	["black", secondary1_1],
+});
